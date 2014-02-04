faa0ffad0d2a3d6a2fc765293badf912aa91bb4a
Alan Gibson <alan.gibson@gmail.com>
2014-02-04 11:43:49 +0100

    Adding public synonyms for TLOG, VLOG, PLOG and LOGMESSAGE Fixed typo

--- sql/install_sys/grant_after_installation.sql
+++ sql/install_sys/grant_after_installation.sql
@@ -3,9 +3,17 @@ set verify off
 ACCEPT V_USER CHAR PROMPT 'Enter the user name:'
 
 GRANT EXECUTE ON &V_USER..PLOG_OUT_AQ TO PUBLIC;
-GRANT EXECUTE ON &V_USER..PLOG_PARAM TO PUBLIC;
+GRANT EXECUTE ON &V_USER..PLOGPARAM TO PUBLIC;
 GRANT EXECUTE ON &V_USER..PLOG TO PUBLIC;
 
+create public synonym plog for plog;
+create public synonym logmessage for logmessage;
+grant execute on logmessage to public;
+
+create public synonym tlog for tlog;
+create public synonym vlog for vlog;
+grant select on tlog to public;
+grant select on vlog to public;
 
 set verify on
 
